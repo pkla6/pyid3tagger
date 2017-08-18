41c96beb2cae35cb5953576ef99f1638e67a5cc8
pkla6 <peter@mediadock.org>
2017-08-18 15:29:31 +0200

    uitlities extended

--- pyid3tagger/utilities.py
+++ pyid3tagger/utilities.py
@@ -1,6 +1,14 @@
 # coding=utf-8
 
+import errno
+import os
+import stat
+
 import pyid3tagger.const as const
+
+__author__ = "Peter Klassen peter@mediadock.org"
+__license__ = "MIT License"
+__copyright__ = "(c) 2017 Peter Klassen peter@mediadock.org"
 
 
 def guess_tags_versions(file_path):
@@ -29,9 +37,47 @@ def guess_tags_versions(file_path):
         elif version_bytes == '\x04\x00':
             versions.append(const.ID3v2_4_VERSION)
 
-    # todo id3v2.4 tag at end of file
+    mp3_file.close()
+
+    if get_ID3_2_4_start_positions(file_path):
+        versions.append(const.ID3v2_4_VERSION)
 
     return tuple(versions)
+
+
+def has_ID3_2_4_begin_tag(file_path):  # todo test
+    mp3_file = open(file_path)
+    return mp3_file.read(5) == '\x73\x68\x51\x04\x00'
+
+
+def get_ID3_2_4_start_positions(file_path):  # todo test
+    """
+
+    :param file_path:
+    :return:
+    """
+    read_length = 1024
+    positions = list()
+    start_positions = list()
+
+    mp3_file = open(file_path)
+    # miss the first 'I'
+    mp3_file.read(1)
+
+    # find the starting 'I'
+    chunk = mp3_file.read(read_length)
+    count = 0
+    while chunk:
+        positions += [position * count + 1 for position, char in enumerate(chunk) if char == 'I']
+        count += 1
+        chunk = mp3_file.read(read_length)
+
+    for position in positions:
+        mp3_file.seek(position)
+        if mp3_file.read(5) == '\x73\x68\x51\x04\x00':
+            start_positions.append(position)
+
+    return tuple(start_positions)
 
 
 def remove_all_tags(file_path, preserve_file_stat=False):
@@ -42,4 +88,54 @@ def remove_all_tags(file_path, preserve_file_stat=False):
     :return:
     """
 
+    remove_ID3_1_tag(file_path, preserve_file_stat)
+
     # todo
+
+
+def remove_ID3_1_tag(file_path, preserve_file_stat=False):  # todo test
+    """
+
+    :param file_path:
+    :param preserve_file_stat:
+    :return:
+    """
+
+    file_stat, mode = _read_file_stat(file_path)
+
+    mp3_file = open(file_path, 'r+')
+    mp3_file.seek(-128, 2)
+    if mp3_file.read(3) == 'TAG':
+        mp3_file.seek(-128, 2)
+        mp3_file.truncate()
+    mp3_file.close()
+
+    if preserve_file_stat:
+        _write_file_stat(file_path, mode)
+
+
+def _read_file_stat(file_path):  # todo test
+    """
+
+    :param file_path:
+    :return:
+    """
+    file_stat = os.stat(file_path)
+    mode = stat.S_IMODE(file_stat.st_mode)
+    return file_stat, mode
+
+
+def _write_file_stat(file_path, file_stat, mode):  # todo test
+    if hasattr(os, 'utime'):
+        os.utime(file_path, (file_stat.st_atime, file_stat.st_mtime))
+    if hasattr(os, 'chmod'):
+        os.chmod(file_path, mode)
+    if hasattr(os, 'chflags') and hasattr(file_stat, 'st_flags'):
+        try:
+            os.chflags(file_path, file_stat.st_flags)
+        except OSError, why:
+            for err in 'EOPNOTSUPP', 'ENOTSUP':
+                if hasattr(errno, err) and why.errno == getattr(errno, err):
+                    break
+            else:
+                raise
--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+import os
+import shutil
 import unittest
 
 import pyid3tagger
@@ -7,11 +9,63 @@ import pyid3tagger
 
 class UtilitiesTest(unittest.TestCase):
 
-    def test_guess_tags_versions_id2v1(self):
+    def test_guess_tags_versions_id3v1(self):
         versions = pyid3tagger.guess_tags_versions('TestData\\id3v1\\id3v1_001_basic.mp3')
-        self.assertTrue(2, len(versions))
+        self.assertEqual(2, len(versions))
         self.assertTrue(pyid3tagger.ID3v1_VERSION in versions)
         self.assertTrue(pyid3tagger.ID3v1_1_VERSION in versions)
+
+    def test_guess_tags_versions_no_tag(self):
+        versions = pyid3tagger.guess_tags_versions('TestData\\no_tag.mp3')
+        self.assertEqual(0, len(versions))
+
+    def test_remove_id3v1_tag(self):
+        source_path = 'TestData\\id3v1\\id3v1_274_extra.mp3'
+        target_path = 'should_have_no_tag.mp3'
+        compare_file_path = 'TestData\\no_tag.mp3'
+
+        if os.path.exists(target_path):
+            os.remove(target_path)
+        shutil.copyfile(source_path, target_path)
+
+        versions = pyid3tagger.guess_tags_versions(target_path)
+        self.assertEqual(2, len(versions))
+        self.assertTrue(pyid3tagger.ID3v1_VERSION in versions)
+        self.assertTrue(pyid3tagger.ID3v1_1_VERSION in versions)
+
+        # remove tag
+        pyid3tagger.remove_ID3_1_tag(target_path)
+
+        versions = pyid3tagger.guess_tags_versions(target_path)
+        self.assertEqual(0, len(versions))
+
+        no_tag_file_content = open(compare_file_path).read()
+        should_have_no_tag_file_content = open(target_path).read()
+
+        self.assertEqual(no_tag_file_content, should_have_no_tag_file_content)
+
+        os.remove(target_path)
+
+    def test_remove_id3v1_tag_no_tag(self):
+        source_path = 'TestData\\no_tag.mp3'
+        target_path = 'should_have_no_tag.mp3'
+
+        if os.path.exists(target_path):
+            os.remove(target_path)
+        shutil.copyfile(source_path, target_path)
+
+        # remove not existing tag
+        pyid3tagger.remove_ID3_1_tag(target_path)
+
+        versions = pyid3tagger.guess_tags_versions(target_path)
+        self.assertEqual(0, len(versions))
+
+        no_tag_file_content = open(source_path).read()
+        should_have_no_tag_file_content = open(target_path).read()
+
+        self.assertEqual(no_tag_file_content, should_have_no_tag_file_content)
+
+        os.remove(target_path)
 
 if __name__ == '__main__':
     unittest.main()
